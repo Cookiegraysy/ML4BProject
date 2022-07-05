ac31e60f22780add5d09d55cd053c6c97f5c8459
Cookiegraysy <50171488+Cookiegraysy@users.noreply.github.com>
2022-07-05 17:30:43 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from pages import information, model_demo, Business_Understanding, Modeling, Evaluation
+from pages import information, model_demo, Modeling, Evaluation
 import streamlit as st
 
 # Page Settings
@@ -32,7 +32,7 @@ st.markdown(
 PAGES = {
     "Model Demo": model_demo,
     "About The Project": information,
-    "Business Understanding: Business_Understanding,
+    #"Business Understanding: Business_Understanding,
     "Modeling": Modeling,
     "Evaluation": Evaluation
 }
