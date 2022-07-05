e20736b28382edffd35a750cf2b6a796d16b81f4
nahihiry <92831472+nahihiry@users.noreply.github.com>
2022-07-05 17:48:42 +0200

    Update Business_Understanding.py

--- pages/Business_Understanding.py
+++ pages/Business_Understanding.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
-with st.expander("1. Business Understanding: ")
+st.expander("1. Business Understanding: ")
 st.title("1. Business Understanding: ")
 st.write("Climate change is a problem that young people, in particular, have been addressing over the last few years. People use vast amounts of packaging waste, etc. and thus destroy our planet step by step. The earth is full of garbage. One step to counter this problem is recycling. In Germany, fortunately, everyone is obliged to recycle properly. In other countries this is unfortunately neglected. Since it is not a compulsory task for many people, they have no idea how to recycle properly. This prevents many people from even trying, resulting in a lot of garbage ending up in our oceans.", True)
