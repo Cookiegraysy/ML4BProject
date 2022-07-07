e43939a157038e6ac5020356d9ea9397e741bdff
Sh1n3se <92376582+Sh1n3se@users.noreply.github.com>
2022-07-07 23:19:10 +0200

    Update 4_Data_Preparation.py

--- pages/4_Data_Preparation.py
+++ pages/4_Data_Preparation.py
@@ -20,7 +20,7 @@ for label in os.listdir(data_dir):
     samples = os.listdir(directory)
 
     for img in samples:
-        z = Image.open(os.path.join(directory, img))
+        z = Image.open(os.path.join(directory, training))
         x = {'Training set': img, 'category': label, 'size': z.size}
         dicts.append(x)
 
@@ -32,7 +32,7 @@ for label in os.listdir(data_dir2):
     samples = os.listdir(directory)
 
     for img in samples:
-        z = Image.open(os.path.join(directory, img))
+        z = Image.open(os.path.join(directory, test))
         x = {'Test set': img, 'category': label, 'size': z.size}
         dicts.append(x)
 
