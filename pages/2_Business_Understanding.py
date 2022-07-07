9b21cb560987343e6845e40bdf7411f8b313f4d1
nahihiry <92831472+nahihiry@users.noreply.github.com>
2022-07-08 00:43:33 +0200

    Update 2_Business_Understanding.py

--- pages/2_Business_Understanding.py
+++ pages/2_Business_Understanding.py
@@ -8,3 +8,7 @@ st.markdown("Climate change is a problem that young people in particularly have 
 st.subheader("Solution:")
 st.markdown("One step to counter this problem is recycling. Fortunately, in Germany everyone is obliged to recycle properly. In other countries, this is unfortunately neglected. Since it is not an obligatory task for many people, they have no idea how to properly dispose and separate their waste in the first place. This keeps many people from even trying to separate their trash, which leads to the fact that the littering of the earth is getting worse and worse.Our app can help and encourage people to take a step into the right direction. Once you are unsure which garbage container a certain garbage needs to go in, our app can identify the garbage on behalf of the person. ")
 st.markdown("The app can also be used by garbage collection points. In this way, no individual has to deal with the separation of garbage, as the garbage is split automatically by our app.")
+st.subheader("Risks:")
+st.markdown("Chances are that the app will misclassify the garbage and therefore the garbage might end up in a garbage can where it doesn't actually belong.")
+st.subheader("Project Plan:")
+st.image()
