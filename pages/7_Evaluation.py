d185a20890707b8abae34a65f0e653b426b5eab7
Sh1n3se <92376582+Sh1n3se@users.noreply.github.com>
2022-07-08 00:00:01 +0200

    Update 7_Evaluation.py

--- pages/7_Evaluation.py
+++ pages/7_Evaluation.py
@@ -4,7 +4,7 @@ import os
 import pandas as pd
 
 st.title("Evaluation")
-st.headliner("Internet Samples")
+st.header("Internet Samples")
 st.markdown("We have uploaded a few examples of each category from the internet into our app.")
 
 categories = ["Select Category", "Cardboard", "Glass", "Metal", "Paper", "Plastic"]
