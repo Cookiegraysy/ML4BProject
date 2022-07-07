4d61fef1409ecaca8048f6be2bd3e9af3dbc1bc7
Cookiegraysy <50171488+Cookiegraysy@users.noreply.github.com>
2022-07-07 22:37:25 +0200

    Add files via upload

---
+++ pages/1_Information.py
@@ -0,0 +1,24 @@
+import streamlit as st
+
+
+
+st.title("Project Garbage Classification")
+
+st.subheader("Objective: ")
+st.markdown(
+    "The main Objective of this project is to use `Machine Learning` to categorize the garbage image into different classes correctly. ",
+    True)
+
+st.markdown("<hr/>", True)
+
+st.subheader("About us: ")
+st.markdown(
+    "Hello, we are Jan, Narin and Siyu. We are student of business informatics and are happy to attend the course ML4B and learn more about the world of machine learning. ",
+    True)
+
+st.markdown("<hr/>", True)
+
+st.subheader("Code: ")
+st.markdown(
+    "Full code and files can be found on our Github if needed [GitHub Repo](https://github.com/Cookiegraysy/ML4BProject)", True)
+
