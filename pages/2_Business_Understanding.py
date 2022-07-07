063f47a20ab994f0f9e1d1e72d14c158ea1b285b
nahihiry <92831472+nahihiry@users.noreply.github.com>
2022-07-07 23:36:41 +0200

    Update 2_Business_Understanding.py

---
+++ pages/2_Business_Understanding.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import pathlib
+import os
+
+st.title("Business Understanding")
+st.subheader("Problem:")
+st.markdown("Climate change is a problem that young people in particularly have been concerned with over the past few years. One of the reasons for this is that we consume huge amounts of packaging waste, etc., and thereby destroy our planet step by step. The earth is full of garbage and it must be changed as soon as possible before it is too late. ")
+st.subheader("Solution:")
