d950bc34249f981e2206a4262bd12a872062978c
Sh1n3se <92376582+Sh1n3se@users.noreply.github.com>
2022-07-08 00:42:14 +0200

    Update 7_Evaluation.py

--- pages/7_Evaluation.py
+++ pages/7_Evaluation.py
@@ -7,9 +7,13 @@ st.title("Evaluation")
 st.header("Internet Samples")
 st.markdown("We have uploaded a few examples of each category from the internet into our app.")
 
+dir = './Result_img/'
+dir2 = './Result/'
+
 categories = ["Select Category", "Cardboard", "Glass", "Metal", "Paper", "Plastic"]
 result = st.selectbox("Select a Category", categories)
-st.write(https://www.hausjournal.net/wp-content/uploads/Pappe-zerkleinern.jpg)
+st.write(./Result_img/cardboard1.jpg)
+
 def cardboard():
   st.write("Sample 1")
-  st.write(https://www.hausjournal.net/wp-content/uploads/Pappe-zerkleinern.jpg)
+  st.write()
