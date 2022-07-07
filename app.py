a3056df189ef2712f2fd48d4e91e89116705af40
Cookiegraysy <50171488+Cookiegraysy@users.noreply.github.com>
2022-07-07 22:38:01 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
-from pages import information, model
 import streamlit as st
-# Page Settings
-favicon = "images/favicon.ico"
-st.set_page_config(page_title="Garbage Classification", page_icon=favicon)
+import predictor
+import plotly.express as px
 
 # Use local CSS
 def local_css(file_name):
@@ -29,12 +27,41 @@ st.markdown(
     """,
     unsafe_allow_html=True,
 )
-PAGES = {
-    "Model Demo": model,
-    "About The Project": information
-}
 
-st.sidebar.title('Garbage Classification')
-selection = st.sidebar.radio("Navigate To", list(PAGES.keys()))
-page = PAGES[selection]
-page.app()
+st.write("# Project Garbage Classification")
+
+st.write("## Upload Image in .jpg format")
+uploaded_image = st.file_uploader("", type=["jpg"])
+
+st.write("## Uploaded Image")
+
+if uploaded_image:
+    st.image(uploaded_image)
+
+    button = st.button("Classify", key=None)
+
+    if button:
+        prediction, predicted_class = predictor.predict(uploaded_image)
+
+        labels = {0: 'cardboard', 1: 'glass', 2: 'metal', 3: 'paper', 4: 'plastic', 5: 'trash'}
+
+        classes = []
+        prob = []
+        for i, j in enumerate(prediction[0], 0):
+            classes.append(labels[i].capitalize())
+            prob.append(round(j * 100, 2))
+
+        fig = px.bar(x=classes, y=prob,
+                     text=prob, color=classes,
+                     labels={"x": "Material", "y": "Probability(%)"})
+
+        st.markdown("#### Probability Distribution Bar Chart", True)
+        st.plotly_chart(fig)
+
+        st.markdown(
+            f"#### The Image Is Classified As`{predicted_class.capitalize()}` With A Probability Of `{max(prob)}%`",
+            True)
+
+else:
+    st.write("#### No Image Was Found, Please Retry!!!")
+
